e86c136ab4348b9590b8b88ee074a2d3cf96ee24
Selvapriya2021 <117925623+Selvapriya2021@users.noreply.github.com>
2022-11-15 10:08:50 +0530

    Update streamlit_app.py
    added a library

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,8 @@
 import pandas
 import streamlit
+import requests
+import snowflake.connector
+from urllib.error import URLError
 streamlit.title('My First Project')
 streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
@@ -14,13 +17,11 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header("Fruityvice Fruit Advice!")
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered ', fruit_choice)
-import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 # write your own comment -what does the next line do? 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # write your own comment - what does this do?
 streamlit.dataframe(fruityvice_normalized)
-import snowflake.connector
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("SELECT * FROM FRUIT_LOAD_LIST")
@@ -28,4 +29,4 @@ my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
 add_my_fruit = streamlit.text_input('What fruit would you like to add?','jacfruit')
-streamlit.text('Thanks for adding jackfruit')
+streamlit.write('Thanks for adding' + add_my_fruit)
